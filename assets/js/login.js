cd5197417a3572b94fec25306cad203fdb07bb99
Prit Manvar <pritmanvar1109@gmail.com>
2022-01-22 14:45:36 +0530

    worked on sign in and sign up button

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,18 +1,21 @@
-let singup = document.querySelector('.singUp');
-let singin = document.querySelector('.singIn');
-let singInPage = document.querySelector('.singInForm');
-let singUpPage = document.querySelector('.singUpForm');
+let signup = document.querySelector('.signUp');
+let signin = document.querySelector('.signIn');
+let signInPage = document.querySelector('.signInForm');
+let signUpPage = document.querySelector('.signUpForm');
 
-singup.addEventListener('click',()=>{
-    singin.style.boxShadow = "inset 5px -3px 5px rgba(0, 0, 0, 0.377)";
-    singup.style.boxShadow = "none";
-    singInPage.style.display = "none";
-    singUpPage.style.display = "block";
+signin.addEventListener('click',()=>{
+    signin.style.boxShadow = "inset 0px 0px 5px 2px rgba(0, 0, 0, 0.377)";
+    signin.style.fontWeight  = "700";
+    signup.style.fontWeight  = "400";
+    signup.style.boxShadow = "none";
+    signInPage.style.display = "none";
+    signUpPage.style.display = "block";
 });
-singin.addEventListener('click',()=>{
-    singup.style.boxShadow = "inset -5px -3px 5px rgba(0, 0, 0, 0.377)";
-    singin.style.boxShadow = "none";
-    console.log(singin.style.boxShadow)
-    singUpPage.style.display = "none";
-    singInPage.style.display = "block";
+signup.addEventListener('click',()=>{
+    signup.style.boxShadow = "inset 0px 0px 5px 2px rgba(0, 0, 0, 0.377)";
+    signup.style.fontWeight  = "700";
+    signin.style.fontWeight  = "400";
+    signin.style.boxShadow = "none";
+    signUpPage.style.display = "none";
+    signInPage.style.display = "block";
 });
