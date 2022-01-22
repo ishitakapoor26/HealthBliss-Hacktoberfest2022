dd99779713b80d6a347baf8653c6606f5983fdc3
Prit Manvar <pritmanvar1109@gmail.com>
2022-01-22 15:06:42 +0530

    minor changes

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,19 +3,19 @@ let signin = document.querySelector('.signIn');
 let signInPage = document.querySelector('.signInForm');
 let signUpPage = document.querySelector('.signUpForm');
 
-signin.addEventListener('click',()=>{
-    signin.style.boxShadow = "inset 0px 0px 5px 2px rgba(0, 0, 0, 0.377)";
-    signin.style.fontWeight  = "700";
-    signup.style.fontWeight  = "400";
-    signup.style.boxShadow = "none";
-    signInPage.style.display = "none";
-    signUpPage.style.display = "block";
-});
 signup.addEventListener('click',()=>{
     signup.style.boxShadow = "inset 0px 0px 5px 2px rgba(0, 0, 0, 0.377)";
     signup.style.fontWeight  = "700";
     signin.style.fontWeight  = "400";
     signin.style.boxShadow = "none";
+    signInPage.style.display = "none";
+    signUpPage.style.display = "block";
+});
+signin.addEventListener('click',()=>{
+    signin.style.boxShadow = "inset 0px 0px 5px 2px rgba(0, 0, 0, 0.377)";
+    signin.style.fontWeight  = "700";
+    signup.style.fontWeight  = "400";
+    signup.style.boxShadow = "none";
     signUpPage.style.display = "none";
     signInPage.style.display = "block";
 });
