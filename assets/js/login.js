8583b1f002ff46f1243ede218b1c909875cc1e0b
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2022-10-12 16:58:59 +0530

    add validation algorithm to login page

--- assets/js/login.js
+++ assets/js/login.js
@@ -19,3 +19,30 @@ signin.addEventListener('click',()=>{
     signUpPage.style.display = "none";
     signInPage.style.display = "block";
 });
+
+function checkSignUp() {
+    var mail_format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var enteredEmail = document.getElementById("signUpEmail");
+    var pwd = document.getElementById("signUpPassword");
+    var conformPwd = document.getElementById("signUpConformPassword");
+    var passFormat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+
+    if(enteredEmail.value.match(mail_format) && pwd.value.match(passFormat) && pwd.value.match(conformPwd.value)) {
+        // successfully validated the fields
+        // rest of the code here..
+        // alert("validated!");
+        return true;
+    }
+    else {
+        if (!enteredEmail.value.match(mail_format)) {
+            alert("Invalid Email!");
+        }
+        else if (!pwd.value.match(conformPwd.value)) {
+            alert("Passwords are not matching!");
+        }
+        else {
+            alert("Enter a Strong password!");
+        }
+    }
+}
